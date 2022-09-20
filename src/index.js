6e679616a0a9b4e4d32da87a7a700c45f0cdc5c0
Dipali127 <dipalibohara500@gmail.com>
2022-09-20 01:56:45 -0600

    project-3

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser'); 
+const route = require('./routes/route');
+const { default: mongoose } = require("mongoose")
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+
+
+mongoose.connect("mongodb+srv://omkar077:BeEHI8wSohCTOfTl@cluster0.tyx7riv.mongodb.net/group14Database",
+
+    {
+        useNewUrlParser: true  //you should set useNewUrlparser :true unless to prevent from connecting.
+    })
+    .then(() => { console.log("MongoDb is connected..."); })
+    .catch(err => console.log(err))
+
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000,function () { 
+    console.log('express app started on the port ' + (process.env.PORT || 3000))});
