70ff0e67afd49692518e7c6bab57ade4b0aad087
Dipali127 <dipalibohara500@gmail.com>
2022-09-24 00:55:32 -0600

    new change

--- src/routes/route.js
+++ src/routes/route.js
@@ -9,17 +9,16 @@ const reviewController = require("../controllers/reviewController")
 
 router.post("/register", userController.createuser)
 router.post("/login", userController.login)
-router.post("/books", bookController.createBooks )//authentication
-router.get("/books", bookController.getBooks)//authentication
-router.get("/books/:bookId", bookController.getBookById)//authenticaion
-router.put("/books/:bookId", bookController.Booksupdate)//authentication nd authorization
+router.post("/books", bookController.createBooks )
+router.get("/books", bookController.getBooks)
+router.get("/books/:bookId", bookController.getBookById)
+router.put("/books/:bookId", bookController.Booksupdate)
 router.delete("/books/:bookId", bookController.deleteBooksbyId)
 router.delete("/books/:bookId/review/:reviewId", reviewController.deleteReview)
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>API's for review >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-router.post("/books/:bookId/review", reviewController.createReview)
-router.put("/books/:bookId/review/:reviewId",reviewController.updateReviewByID)
-router.delete("/books/:bookId/review/:reviewId", reviewController.deleteReview)
+// router.post("/books/:bookId/review", reviewController.createReview)
+// router.put("/books/:bookId/review/:reviewId",reviewController.updateReviewByID)
+// router.delete("/books/:bookId/review/:reviewId", reviewController.deleteReview)
 
 
 router.all("/*",(req,res)=>{res.status(404).send({status:false,message:"Invalid path params"})})
